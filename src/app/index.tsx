dc251acec9fe9bbb7d0c42f5f8eca3970780a9ee
Jessica Caroline <jessicarolyne@hotmail.com>
2024-02-07 23:25:39 -0300

    Aula 1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
-import { View, Text } from "react-native";
+//import React from 'react'
+import { View, Text } from "react-native"
+
 export default function Home(){
   return(
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white">Olá</Text>
-    </View>
-  )
+    <View className="flex-1">
+      <Text className="text-white m-auto text-lg">Olá Jessica</Text>
+    </View> 
+  ) 
 }
